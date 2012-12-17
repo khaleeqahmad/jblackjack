42d068a292cf4887945fb81e40767c01a3b745c1
Khaleeq Ahmad <kahmad28@gmail.com>
2012-12-17 20:43:00 +0000

    Created Player class

---
+++ jblackjack/src/Player.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Khaleeq Ahmad
+ *
+ */
+public class Player
+{
+    String name;
+    int score;
+    
+    Hand hand = new Hand();
+    
+   Player(String name)
+   {
+       this.name = name;
+   }
+}
